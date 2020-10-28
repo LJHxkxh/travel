b4ff9836fefd5fe32c2767a95227ef04c751c20f
L.J.H <745669764@qq.com>
2020-10-28 21:11:33 +0800

    city-router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="header">
+    城市选择
+    <router-link to='/'>
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+	</div>
+</template>
+
+<script>
+
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style scoped>
+  .header {
+    position: relative;
+    height: 2.4rem;
+    line-height: 2.4rem;
+    font-size: 1.05rem;
+    text-align: center;
+    background: #00bcd4;;
+    color: #fff;
+  }
+  .header-back {
+    position: absolute;   /*不进行定位，图标将无法显示*/
+    top: 0;
+    left: 0.3rem;
+    font-size: 1.1rem;
+    color: #fff;
+  }
+</style>
+
+
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -6,9 +6,11 @@
     <div class="header-input">
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题</div>
-    <div class="header-right">
-      {{this.city}}<span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}<span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
 	</div>
 </template>
 
@@ -46,6 +48,7 @@ export default {
     border-radius: .2rem;
   }
   .header-right {
+    color: #fff;
     width: 3rem;
     float: right;
     margin-right: 0.24rem;
