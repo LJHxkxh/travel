422e2580e49fca36e1d89aed1f7b8746e5c5d68d
L.J.H <745669764@qq.com>
2020-10-28 21:40:35 +0800

    city-search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="search">
+    <input class="search-input" type="text" placeholder="请输入城市名称">
+	</div>
+</template>
+
+<script>
+
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style scoped>
+  .search {
+    height: 2rem;
+    background: #00bcd4;
+  }
+  .search-input {
+    width: 96%;
+    height: 1.5rem;
+    line-height: 1.5rem;
+    margin:0 0.29rem;
+    text-align: center;
+    font-size: 0.9rem;
+    border-radius: 0.3rem;
+    border: #00bcd4 solid 0.0625rem;
+  }
+</style>
+
