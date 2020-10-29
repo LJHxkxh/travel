a59369e0ffc98fcc32d99237a4a02fce5e4860e7
L.J.H <745669764@qq.com>
2020-10-29 13:59:43 +0800

    city-list

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,56 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style scoped>
+  .list {
+    /* display: flex;
+    flex-direction:  column;
+    justify-content: center; */
+    position: absolute;
+    right: 0.4rem;
+    top: 15rem;
+  }
+  .item {
+    text-align: center;
+    line-height: 1rem;
+    font-size: 0.6rem;
+    color: #00bcd4;;
+  }
+</style>
+
+
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,156 @@
+<template>
+	<div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">南昌</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">南昌</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">南昌</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">南昌</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">南昌</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+        </div>
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+        </div>
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+        </div>
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+        </div>
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+        </div>
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+        </div>
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+        </div>
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+          <div class="item border-topbottom">啦啦啦</div>
+        </div>
+      </div>
+	  </div>
+  </div>
+</template>
+
+<script>
+import BScroll from '@better-scroll/core'
+export default {
+  name: 'CityList', 
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper)
+  }
+}
+</script>
+
+<style scoped>
+  .border-topbottom:brfore {
+    border-color: #ccc;
+  }
+  .border-topbottom:after {
+    border-color: #ccc;
+  }
+  .list {
+    overflow: hidden;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 4.4rem;
+    bottom: 0;
+  }
+  .title {
+    line-height: 1.4rem;
+    font-size: 0.95rem;
+    padding-left: 0.35rem;
+    padding-top: 0.04rem;
+    padding-bottom: 0.04rem;
+    background: #eee;
+    color: #666;
+  }
+  .button-list {
+    overflow: hidden;     /*触发BFC, 防止父元素高度塌陷*/
+    padding: 0.4rem 0.2rem;
+  }
+  .button-wrapper {
+    float: left;
+    width: 29%;
+    margin: 0.2rem 0.15rem;
+  }
+  .button {
+    text-align: center;
+    padding: 0.2rem 0;
+    border: #ccc solid 0.0625rem;
+    border-radius: 0.25rem;
+  }
+  .item-list {
+    padding-left: 0.35rem;
+    line-height: 1.8rem;
+    font-size: 1rem;
+  }
+
+</style>
+
+
